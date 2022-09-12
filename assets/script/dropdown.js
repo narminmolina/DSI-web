97dde87adc1ff9a8b7f6ab04dbcc0d3b6e92acad
rommelmamedov <rommelmamedov@gmail.com>
2022-09-12 13:42:12 +0400

    Fixes

--- assets/script/dropdown.js
+++ assets/script/dropdown.js
@@ -1,13 +1,19 @@
-document.addEventListener("click", (e) => {
-  let isDropdownButton = e.target.matches("[data-dropdown-button]");
-  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return;
-  let currentDropdown;
-  if (isDropdownButton) {
-    currentDropdown = e.target.closest("[data-dropdown]");
-    currentDropdown.classList.toggle("active");
+document.addEventListener('click', (e) => {
+  let dropdownButton = e.target.matches('[data-dropdown-button]');
+
+  if (!dropdownButton && e.target.closest('[data-dropdown]') !== null) {
+    return;
   }
-  document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
+
+  let currentDropdown;
+
+  if (dropdownButton) {
+    currentDropdown = e.target.closest('[data-dropdown]');
+    currentDropdown.classList.toggle('active');
+  }
+
+  document.querySelectorAll('[data-dropdown].active').forEach((dropdown) => {
     if (dropdown === currentDropdown) return;
-    dropdown.classList.remove("active");
+    dropdown.classList.remove('active');
   });
 });
